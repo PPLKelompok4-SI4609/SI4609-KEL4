a55731f47a167c17183b0c1e810d29ef625d14df
suzanaya <suzannayaputri@gmail.com>
2025-04-28 16:15:53 +0700

    testt

--- LaravelPPLKel4/vendor/composer/installed.php
+++ LaravelPPLKel4/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'd2db465254c59528c6b8c1bf7097b5e3c0f3dfb6',
+        'reference' => 'd6d118fbf331a2d42c90e41abcaa25fe68713e20',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -382,7 +382,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'd2db465254c59528c6b8c1bf7097b5e3c0f3dfb6',
+            'reference' => 'd6d118fbf331a2d42c90e41abcaa25fe68713e20',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -757,8 +757,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '3.0.0',
-                1 => '1.0|2.0|3.0',
+                0 => '1.0|2.0|3.0',
+                1 => '3.0.0',
             ),
         ),
         'psr/simple-cache' => array(
